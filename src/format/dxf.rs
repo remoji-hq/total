de5847decff36d420ec35d16f1c3ae6fc8b112a4
Shamil <66209982+shamilsan@users.noreply.github.com>
2025-11-21 00:32:24 +0300

    feat: add layers to DXF

--- src/format/dxf.rs
+++ src/format/dxf.rs
@@ -1,11 +1,20 @@
-use std::{io, path::Path};
+use std::{collections::HashMap, io, path::Path};
 
 use dxf::{
-    Drawing, DxfError, Point,
-    entities::{Entity, EntityType, Line, Text},
+    Color, Drawing, DxfError, Point,
+    entities::{Circle, Entity, EntityCommon, EntityType, Line, Text},
+    enums::{DrawingUnits, UnitFormat, Units},
+    tables::Layer,
 };
 
 use crate::format::Object;
+
+const POINTS_LAYER: &str = "POINTS";
+const TEXT_LAYER: &str = "TEXT";
+
+const COLOR_INDEX_RED: u8 = 1;
+const COLOR_INDEX_YELLOW: u8 = 2;
+const COLOR_INDEX_GREEN: u8 = 3;
 
 pub struct DxfReader;
 
@@ -27,31 +36,68 @@ impl DxfReader {
 impl DxfWriter {
     pub fn new(objects: Vec<Object>) -> Self {
         let mut drawing = Drawing::new();
+        drawing.header.unit_format = UnitFormat::Architectural;
+        drawing.header.drawing_units = DrawingUnits::Metric;
+        drawing.header.default_drawing_units = Units::Meters;
 
-        let mut last_point = None;
+        drawing.add_layer(Layer {
+            name: POINTS_LAYER.to_string(),
+            color: Color::from_index(COLOR_INDEX_RED),
+            ..Default::default()
+        });
+
+        drawing.add_layer(Layer {
+            name: TEXT_LAYER.to_string(),
+            color: Color::from_index(COLOR_INDEX_YELLOW),
+            ..Default::default()
+        });
+
+        let mut last_points = HashMap::new();
+        let mut color_index = COLOR_INDEX_GREEN;
+
         for object in objects {
             match object {
-                Object::Point { n, e, z, name, .. } => {
+                Object::Point {
+                    n,
+                    e,
+                    z,
+                    name,
+                    code,
+                } => {
+                    drawing.add_entity(point_circle(n, e, z));
                     if !name.is_empty() {
-                        drawing.add_entity(Entity::new(EntityType::Text(Text {
-                            location: Point { x: n, y: e, z },
-                            text_height: 1.0,
-                            value: name,
-                            ..Default::default()
-                        })));
+                        drawing.add_entity(point_text(n, e, z, name));
                     }
-                    if let Some((ln, le, lz)) = last_point {
-                        drawing.add_entity(Entity::new(EntityType::Line(Line {
-                            p1: Point { x: n, y: e, z },
-                            p2: Point {
-                                x: ln,
-                                y: le,
-                                z: lz,
+                    let layer = if code.is_empty() {
+                        "0".to_string()
+                    } else {
+                        code.clone()
+                    };
+                    if let Some((last_n, last_e, last_z)) = last_points.get(&code) {
+                        drawing.add_entity(Entity {
+                            common: EntityCommon {
+                                layer,
+                                ..Default::default()
                             },
+                            specific: EntityType::Line(Line {
+                                p1: Point { x: n, y: e, z },
+                                p2: Point {
+                                    x: *last_n,
+                                    y: *last_e,
+                                    z: *last_z,
+                                },
+                                ..Default::default()
+                            }),
+                        });
+                    } else if !code.is_empty() {
+                        drawing.add_layer(Layer {
+                            name: layer,
+                            color: Color::from_index(color_index),
                             ..Default::default()
-                        })));
+                        });
+                        color_index += 1;
                     }
-                    last_point = Some((n, e, z));
+                    last_points.insert(code, (n, e, z));
                 }
             }
         }
@@ -64,5 +110,34 @@ impl DxfWriter {
             DxfError::IoError(err) => err,
             err => io::Error::other(err),
         })
+    }
+}
+
+fn point_text(x: f64, y: f64, z: f64, value: String) -> Entity {
+    Entity {
+        common: EntityCommon {
+            layer: TEXT_LAYER.to_string(),
+            ..Default::default()
+        },
+        specific: EntityType::Text(Text {
+            location: Point { x, y, z },
+            text_height: 0.5,
+            value,
+            ..Default::default()
+        }),
+    }
+}
+
+fn point_circle(x: f64, y: f64, z: f64) -> Entity {
+    Entity {
+        common: EntityCommon {
+            layer: POINTS_LAYER.to_string(),
+            ..Default::default()
+        },
+        specific: EntityType::Circle(Circle {
+            center: Point { x, y, z },
+            radius: 0.1,
+            ..Default::default()
+        }),
     }
 }
